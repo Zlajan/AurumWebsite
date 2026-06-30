d9474574b8d46375dc4ae425b9339cc9e0798b10
Zlajan <zlatkojovovic960@gmail.com>
2026-06-30 21:28:23 +0200

    Site template

--- js/script.js
+++ js/script.js
@@ -1,1 +1,75 @@
-console.log("Aurum Website");
+window.addEventListener("DOMContentLoaded", () => {
+
+    const header = document.querySelector(".header");
+    const reveals = document.querySelectorAll(".reveal");
+
+    // HEADER SCROLL
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+
+        revealOnScroll(); // važno
+    });
+
+    // REVEAL FUNCTION
+    function revealOnScroll() {
+        const windowHeight = window.innerHeight;
+
+        for (let i = 0; i < reveals.length; i++) {
+            const elementTop = reveals[i].getBoundingClientRect().top;
+            const elementVisible = 120;
+
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            }
+        }
+    }
+
+    // RUN ON LOAD (KLJUČNO)
+    revealOnScroll();
+
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        toTop.classList.add("show");
+    } else {
+        toTop.classList.remove("show");
+    }
+});
+
+toTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
